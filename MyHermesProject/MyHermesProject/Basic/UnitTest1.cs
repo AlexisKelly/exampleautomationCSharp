5c67546a4f12ae0a8538efaf369a59b18914cd67
alexis.kelly <alexis.kelly@bjss.co.uk>
2019-02-19 10:22:25 +0000

    Adding initial project files and renamed gitignore

---
+++ MyHermesProject/MyHermesProject/Basic/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MyHermesProject.Basic
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        public IWebDriver driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.myhermes.co.uk/");
+        }
+    }
+}
---
+++ MyHermesProject/MyHermesProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyHermesProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyHermesProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("343f7f48-5470-4c2d-93ed-3027e58a0b57")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
